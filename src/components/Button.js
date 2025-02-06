e2f3ff48fd976d2264655aab146ab974dd788e75
beunyeong <beunyeong.b@gmail.com>
2025-02-06 19:51:53 +0900

    Style : 버튼 기본 색상을 회색으로 변경

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,8 @@ const Button = ({ onClick, children }) => {
     return (
       <button
         onClick={onClick}
-        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-      >
+        className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+        >
         {children}
       </button>
     );
