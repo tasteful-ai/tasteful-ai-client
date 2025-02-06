483a456eaa699ac0b707b9dca18f562253539953
beunyeong <beunyeong.b@gmail.com>
2025-02-06 19:45:06 +0900

    Feat : 재사용 가능한 Button 및 Input 컴포넌트 추가

---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+const Input = ({ type, placeholder, value, onChange }) => {
+    return (
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="border p-2 rounded-md w-full"
+      />
+    );
+  };
+  
+  export default Input;
+  
